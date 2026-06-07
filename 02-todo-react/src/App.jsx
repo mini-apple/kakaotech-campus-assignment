8e8cc0bec9456f99aee05cca3debb6da0224b61c
mini-apple <suc2150@gmail.com>
2026-06-07 16:24:44 +0900

    chore: Vite + React, Tailwind CSS v4 세팅

---
+++ 02-todo-react/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="max-w-[560px] mx-auto px-4 py-6">
+      <h1 className="text-2xl font-bold text-primary">My Todo</h1>
+    </div>
+  )
+}
+
+export default App
